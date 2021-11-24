0fd0ea34c579dac2e4da4fe089c7448937cc3a3f
languantan <languan.tan@gmail.com>
2021-11-24 00:35:23 -0800

    fix: update response structure

--- model/model.py
+++ model/model.py
@@ -15,5 +15,5 @@ def paraphrase_sentence(input_text,num_return_sequences=1,num_beams=20):
   batch = tokenizer([input_text],truncation=True,padding='longest',max_length=60, return_tensors="pt").to(torch_device)
   translated = model.generate(**batch,max_length=60,num_beams=num_beams, num_return_sequences=num_return_sequences, temperature=1.5)
   tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
-  return tgt_text
+  return tgt_text[0]
   
